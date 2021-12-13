ccbd23133933835fb6bb2a4b719348b953542ca2
Guillaume Dube <gdube@sajb.ca>
2021-12-12 22:29:24 -0500

    Update search code

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,11 +1,14 @@
 var summaryInclude = 60 * 3; // Chars to include per result.
 var fuse = null;
 var fuseOptions = {
+    tokenize: true,
     shouldSort: true,
     includeMatches: true,
-    threshold: 0.0,
+    threshold: 1,
     location: 0,
     distance: 100,
+    ignoreLocation: true,
+    ignoreFieldNorm: true,
     minMatchCharLength: 3,
     keys: [
         {name: "title", weight: 0.8},
@@ -14,13 +17,13 @@ var fuseOptions = {
         // {name: "categories", weight: 0.3}
     ]
 };
-
+let domParser = new DOMParser();
 let tbSearchID = "search-query";
 let tbSearch = document.getElementById(tbSearchID);
 let searchResultsID = 'search-results';
 let searchResults = document.getElementById(searchResultsID);
-let templateEmpty = document.getElementById("search-result-template-empty");
-let searchResultTemplate = document.getElementById("search-result-template");
+let templateEmpty = document.getElementById("search-result-empty");
+let templateSearchResult = document.getElementById("search-result");
 
 function init() {
     if (!tbSearch) {
@@ -31,7 +34,7 @@ function init() {
         console.log(`Panel '${searchResultsID}' not found.`);
         return;
     }
-    onKeyDown = debounce((event) => {
+    onKeyDown = debounce(async (event) => {
             // console.log('Searching')
             if (event.key === "Escape") {
                 tbSearch.value = "";
@@ -40,9 +43,12 @@ function init() {
             }
             else if (event.key === "Enter") 
                 return;
+            
             let query = tbSearch.value;
             if (!query) toggleResultBox(open = false);
-            else search(query, false);
+            resultNodes = await search(query);
+            searchResults.replaceChildren(...resultNodes);
+            toggleResultBox(true);
     });
     tbSearch.addEventListener('keydown', onKeyDown);
     // --
@@ -68,12 +74,7 @@ function toggleResultBox(open) {
     else { searchResults.classList.add("close"); }
 }
 
-function clearResultBox() {
-    searchResults.innerHTML = templateEmpty.innerHTML;
-}
-
 async function search(query) {
-    hasResults = false
     try {
         if (fuse == null) {
             // console.log('Fetching search index');
@@ -88,54 +89,66 @@ async function search(query) {
             pages = await response.json(); 
             fuse = new Fuse(pages, fuseOptions);
         }
-        let results = fuse.search(query);
-        if (results.length > 0) {
-            hasResults = true;
-            populateResults(results, query);
+        let results = await fuse.search(query);
+        let nodes = [];
+        for(const x of results) {
+            // ---------------
+            // x.item.categories
+            // x.item.contents
+            // x.item.tags
+            // x.item.permalink
+            // x.item.title
+            // x.refIndex
+            // x.matches = [ {indices:[[startIdx,endIdx]], key:"<itemProperty>", value:"<queryTerm>" ]
+            // ---------------
+            let item = x.item;
+            let itemEx = {
+                title: item.title,
+                link: item.permalink,
+                // DUBEG: not sure it's a good idea to use the domParser.
+                snippet: domParser.parseFromString(
+                    item.contents.substring(0, summaryInclude),
+                    'text/html'
+                ).body.innerText
+            };
+            let rootNode = templateSearchResult.content.firstElementChild.cloneNode(true);
+            let stack = [];
+            stack.push(rootNode);
+            while (stack.length > 0) {
+                let node = stack.pop();
+                if (node.nodeType === Node.TEXT_NODE) {
+                    node.nodeValue = formatString(node.nodeValue, itemEx);
+                }
+                if (node.attributes && node.attributes.length) {
+                    for (let i = 0; i < node.attributes.length; i += 1) {
+                        let attr = node.attributes[i];
+                        if (attr.specified) {
+                            attr.value = formatString(attr.value, itemEx);
+                        }
+                    }
+                }
+                if (node.childNodes && node.childNodes.length) {
+                    for (let i = 0; i < node.childNodes.length; i += 1) {
+                        stack.push(node.childNodes[i]);
+                    }
+                }
+            }
+            nodes.push(rootNode);
         }
-        toggleResultBox(true);
+        if (nodes.length === 0)
+            nodes.push(templateEmpty.content.firstElementChild.cloneNode(true));
+        return nodes;
     }
     catch(error) {
         console.log(error);
     }
-    finally {
-        if (!hasResults) 
-            clearResultBox();
-    }
 }
 
-function populateResults(results, queryToHighlight) {
-    let searchQuery = queryToHighlight;
-    let templateDefinition = searchResultTemplate.innerHTML;
-    htmlResult = "";
-    console.log(results);
-    results.forEach(function (value, key) {
-        // ---------------
-        let matches = value.matches
-        // matches = [ {indices:[[startIdx,endIdx]], key:"<itemProperty>", value:"<queryTerm>" ]
-        // ---------------
-        let item = value.item;
-        let contents = item.contents;
-        let snippet = contents.substring(0, summaryInclude) + '&hellip;';
-        let output = render(templateDefinition, {
-            key: key,
-            title: item.title,
-            link: item.permalink,
-            snippet: snippet
-        });
-        htmlResult += output;
-    });
-    // TODO: add mark.js (highlight matches)
-    searchResults.innerHTML = htmlResult;
-}
-
-function render(templateString, data) {
-    let x = templateString;
-    x = x.replaceAll('{key}', data.key);
-    x = x.replaceAll('{title}', data.title);
-    x = x.replaceAll('{link}', data.link);
-    x = x.replaceAll('{snippet}', data.snippet);
-    return x;
+function formatString(str, item) {
+    str = str.replace("{title}", item.title);
+    str = str.replace("{snippet}", item.snippet);
+    str = str.replace("{link}", item.link);
+    return str;
 }
 
 function debounce(func, timeout = 200){
